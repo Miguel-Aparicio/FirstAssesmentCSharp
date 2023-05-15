22e326e11b052ded2fc0305f723d78a9e07f6cda
Miguel-Aparicio <miguel.apgd@gmail.com>
2023-05-15 09:43:22 +0200

    First Commit

---
+++ clases/class_mexico.cs
@@ -0,0 +1,17 @@
+﻿using Paisbase;
+namespace paismexico;
+
+public class Mexico : Pais
+{
+    private string idioma;
+
+    public Mexico(string nombre, string capital, string idioma) : base(nombre, capital)
+    {
+        this.idioma = idioma;
+    }
+
+    public override string Descripcion()
+    {
+        return $"México es un país de América del Norte, cuya capital es {capital} y su idioma oficial es el {idioma}.";
+    }
+}
---
+++ clases/class_pais.cs
@@ -0,0 +1,15 @@
+﻿namespace Paisbase;
+
+public abstract class Pais
+{
+    protected string nombre;
+    protected string capital;
+
+    public Pais(string nombre, string capital)
+    {
+        this.nombre = nombre;
+        this.capital = capital;
+    }
+
+    public abstract string Descripcion();
+}
